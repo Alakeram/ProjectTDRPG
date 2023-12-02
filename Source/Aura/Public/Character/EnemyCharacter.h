defb3bac614a668fdaee197e0c1f0db34498613c
Michael Moris <evilgeniusdevelopment@gmail.com>
2023-12-02 17:32:13 -0600

    Added enemy and player characters

---
+++ Source/Aura/Public/Character/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Copyright Evil Genius Development.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Copyright Evil Genius Development.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API APlayerCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
